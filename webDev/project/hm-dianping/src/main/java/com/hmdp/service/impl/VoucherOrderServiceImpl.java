a6297e4844cc539c2dd3d61147573cb533ef5d67
Ada-Church-Closure <2574094394@qq.com>
2025-10-28 14:11:54 +0800

    fix(修复优惠券秒杀的问题:乐观锁搭配悲观锁解决,给整个创建订单和修改数据库的事务加悲观锁)/problem(没有引入分布式锁,无法解决负载均衡带来的一人一单无法实现的问题)

--- webDev/project/hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ webDev/project/hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -10,6 +10,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
 import com.sun.corba.se.spi.servicecontext.ORBVersionServiceContext;
+import org.springframework.aop.framework.AopConfigException;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,8 +40,8 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Autowired
     private ResourceTransactionManager resourceTransactionManager;
 
-    @Override
-    @Transactional // 一个秒杀操作一定是一个事务
+
+    @Override   // 一个秒杀操作一定是一个事务
     public Result secKillVoucher(Long voucherId) {
         // 1.查询优惠券
         SeckillVoucher voucher = iSeckillVoucherService.getById(voucherId);
@@ -55,31 +57,58 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         // 4.库存是否充足
+        // 这里可能是多线程不安全的.
         if (voucher.getStock() < 1){
             return Result.fail("库存不足了!!!");
         }
 
         // 5.减去库存
         boolean success = iSeckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .setSql("stock = stock - 1") // set stock = stock - 1
+                .eq("voucher_id", voucherId).gt("stock", 0) // 只要更新时候的value和查询的时候是一样的即可/其实只要更新前大于0即可,否则会出问题
+                .update();
 
         if(!success){
             return Result.fail("库存不足了!!!");
         }
 
-        // 6.创建一个订单
-        // 全局唯一订单ID
-        VoucherOrder voucherOrder = new VoucherOrder();
-        long orderId = redisIdWorker.nextId("order");
-        voucherOrder.setId(orderId);
-        // 用户ID
         Long userId = UserHolder.getUser().getId();
-        voucherOrder.setUserId(userId);
-        // 代金券ID
-        voucherOrder.setVoucherId(voucherId);
-        save(voucherOrder);
+        // lock放在这里是因为一定要等到整个事务提交之后,才能去释放lock.
+        // 事务没有提交,也就没有写到数据库内部,此时可能产生问题.
 
-        return Result.ok(orderId);
+        // 不能给整个this方法上lock,只给一个user上锁
+        // 我们要保证,只要是来自于一个用户的请求,使用的就是一把lock
+        // intern保证持有的lock取决于字符串的常量,而不是一个内存中的新对象
+        synchronized (userId.toString().intern()) {
+            // return createVoucherOrder(voucherId);
+            // 这样直接调用,事务是不会生效的.要使用代理对象.
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    // 直接给整个逻辑上悲观锁
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 先进行一人一单地查询逻辑, 这里的查询也是会出现超卖的问题
+        Long userId = UserHolder.getUser().getId();
+
+            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if (count > 0) {
+                // 存在,表示用户已经购买
+                return Result.fail("用户已经购买了,别当黄牛!!!");
+            }
+            // 6.创建一个订单
+            // 全局唯一订单ID
+            VoucherOrder voucherOrder = new VoucherOrder();
+            long orderId = redisIdWorker.nextId("order");
+            voucherOrder.setId(orderId);
+            voucherOrder.setUserId(userId);
+            // 代金券ID
+            voucherOrder.setVoucherId(voucherId);
+            save(voucherOrder);
+
+            return Result.ok(orderId);
+
     }
 }
